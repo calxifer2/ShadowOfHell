993db9c1d9c0c9fa6cf887e14da2edb8160fb14e
mateo <lu9smateo@gmail.com>
2024-03-07 15:31:34 -0500

    primeros cambios dek proyeccto

---
+++ Assets/script/Movimiento.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public bool enPiso;
+
+
+
+    public void saltar()
+    {
+        if(enPiso)
+        {
+           rb2D.velocity = (collider2D collision)
+        }
+    }
+    privsate void OnTriggerStay2D()
+}
